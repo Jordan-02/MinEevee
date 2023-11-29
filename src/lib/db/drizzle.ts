1c03a3aaab1e7ac608884116f004d30b4ae540da
Jordan Jones <Jordan.M.Jones.24@dartmouth.edu>
2023-11-29 16:26:31 -0500

    Vercel fixes

--- src/lib/db/drizzle.ts
+++ src/lib/db/drizzle.ts
@@ -1,9 +1,10 @@
-import { PUBLIC_DB_URL } from '$env/static/public';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-if (!PUBLIC_DB_URL) throw new Error('Missing env variable "DATABASE_URL"');
+const url = import.meta.env.VITE_DB_URL;
+console.log(url);
+if (!url) throw new Error('Missing env variable "DATABASE_URL"');
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(PUBLIC_DB_URL);
+const client = postgres(url);
 export const db = drizzle(client);
